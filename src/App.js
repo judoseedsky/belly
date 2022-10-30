fcb1b54dc5fc9c916fed28f1bd2178422b4ebde4
Judo Sky <judoseedsky@gmail.com>
2022-10-30 13:55:57 -0400

    loading screen

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    setTimeout(() => setLoading(false), 6000)
+    setTimeout(() => setLoading(false), 3000)
   }, [])
 
   return (
