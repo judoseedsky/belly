a0754ab5745b770ae111f60e9350fe7fb816420f
Judo Sky <judoseedsky@gmail.com>
2022-10-30 13:36:13 -0400

    loading screen

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
 
   return (
     <>
-    {/* {isLoading === false ? */}
+    {isLoading === false ?
       <div className="App">
         <Router>
           <Routes>
@@ -49,9 +49,9 @@ function App() {
         </Router>
         <img src={belly} id="belly" alt=""/>
       </div>
-    {/* : 
+      : 
       <LoadingScreen/>
-    } */}
+    }
     </>
   );
 }
